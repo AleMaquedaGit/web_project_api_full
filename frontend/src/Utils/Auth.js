fae466ac215f0b8654dddc1b52a28728146bb50a
AleMaquedaGit <alealemaqueda@gmail.com>
2026-07-27 10:52:54 -0600

    correc camb Auth

--- frontend/src/Utils/Auth.js
+++ frontend/src/Utils/Auth.js
@@ -41,12 +41,10 @@ class Auth {
   }
 }
 
-const auth = new Auth({
+export const auth = new Auth({
   baseUrl: "https://se-register-api.en.tripleten-services.com/v1",
   headers: {
     authorization: "06f1087a-ea72-4331-b20a-47ee42c926d9",
     "Content-Type": "application/json",
   },
 });
-
-export default auth;
